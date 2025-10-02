c460ad8601cc773130e323a1bb7b16691439f47e
Umang Khemka <umangkhemka2005@gmail.com>
2025-10-02 19:57:59 +0530

    solving error

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,9 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/",(req,res)=>{
+  res.send("Api is running");
+})
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/games",gameRoutes);
