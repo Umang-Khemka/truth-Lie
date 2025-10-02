3959ede528fd1954ca3367ddfcb3c2e067726aa5
Umang Khemka <umangkhemka2005@gmail.com>
2025-10-02 19:53:13 +0530

    solving error

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,11 +26,11 @@ app.use("/api/v1/games",gameRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
 
 
 
